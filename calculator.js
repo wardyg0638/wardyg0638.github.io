daae47b9d356493d6b64bc602c0b603020d3ffb2
Ward (張文京) <Ward@ilepb.gov.tw>
2020-03-12 09:57:58 +0800

    1

---
+++ calculator.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#app',
+    data: {
+        value: '',
+        dB1: '',
+        dB2: ''
+    },
+    methods: {
+        Calculation() {
+            var h = `${this.dB1}`;
+            var w = `${this.dB2}`;
+            var t = 10 * ((Math.log(Math.pow(10, (0.1 * h)) + Math.pow(10, (0.1 * w)))) / (Math.log(10)));
+            this.value = t
+        }
+    },
+})
